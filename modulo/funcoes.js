f26cfc4bc6edb6e7fadd51a1c547e7786b47fa70
GABRIEL LACERDA CORREIA <25132902@CFP127ALUNO.LOCAL>
2025-09-30 10:07:23 -0300

    feat(funções, app): Adicionando casos de erro nas funções e estrutura básica do app

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -5,18 +5,12 @@
  * Versão: 1.0
 *********************************************************************/
 
-const { get } = require('http')
 const dataUser = require("./contatos.js")
 
 const MESSAGE_ERROR = [
     {
         "status": false,
         "status_code": 500,
-        "development": "Gabriel Lacerda Correia"
-    },
-    {
-        "status": false,
-        "status_code": 404,
         "development": "Gabriel Lacerda Correia"
     }
 ]
@@ -32,55 +26,71 @@ const getAllDataUsers = () => {
         result: dataUser.contatos['whats-users']
     }
 
+    let result = message.result === "" ?  MESSAGE_ERROR : message
 
-    return message
+    return result
 }
 
 // Faz a listagem dos dados do usuário através do nickname
-const getAllDataUser = (nickname) => {
+const getAllDataUser = (cellphoneNumber) => {
     
     let message = {
         status: true,
         status_code: 200,
         development:"Gabriel Lacerda Correia", 
-        user: {
+        user: []
+    }
 
-        }
+
+    if (cellphoneNumber === "") {
+        return MESSAGE_ERROR
     }
 
     dataUser.contatos['whats-users'].forEach((user) => {
 
-        if (user.nickname === nickname) {
 
-            message.user.nome = user.account
-            message.user.nome_usuario = user.nickname
-            message.user.numero = user.number
-            message.user.foto_perfil = user['profile-image']
-            message.user.cor_fundo = user.background
-            message.user.data_ingresso_saída = user['created-since']
+        if (user.number === cellphoneNumber) {
 
-        }
+            let account = user.account
+            let username = user.nickname
+            let number  = user.number
+            let profile_photo = user['profile-image']
+            let background = user.background
+            let data_ingresso_saida = user['created-since']
 
+            message.user.push(
+                {
+                    account,
+                    username,
+                    number,
+                    profile_photo,
+                    background,
+                    data_ingresso_saida
+                }
+            )
+        } 
     })
 
-    return message
+    let result = message.user.length < 1 ? MESSAGE_ERROR : message
+
+    return result
 }
 
 // Faz a listagem dos dados de todos os contatos do usuário
-const getAllDataContacts = (nickname) => {
+const getAllDataContacts = (cellphoneNumber) => {
 
     let message = {
         status: true,
         status_code: 200,
         development:"Gabriel Lacerda Correia", 
-        user: {nickname},
-        contacts: [
-
-        ]
+        contacts: []
     }
 
     dataUser.contatos['whats-users'].forEach((user) => {
-        if (user.nickname === nickname) {
+        if (user.number === cellphoneNumber) {
+
+            message.user = user.nickname
+
             user.contacts.forEach((contact) => {
 
                 let name = contact.name
@@ -90,28 +100,29 @@ const getAllDataContacts = (nickname) => {
                 message.contacts.push({ name, photo, description })
 
             })
-        }
+        } 
     })
 
-    return message
+
+    let result = message.contacts.length < 1 ? MESSAGE_ERROR : message
+    return result
 
 }
 
 // Faz a listagem de todas as mensagens trocadas com os 
-// contatos de respectivo usuário pelo nickname
-const getAllMessagesUser = (nickname) => {
+// contatos de usuário pelo nickname
+const getAllMessagesUser = (cellphoneNumber) => {
 
     let message = {
         status: true,
         status_code: 200,
         development:"Gabriel Lacerda Correia", 
-        user: {nickname},
-        contacts: [ ]
+        contacts: []
     }
 
     dataUser.contatos['whats-users'].forEach((user) => {
 
-        if (user.nickname === nickname) {
+        if (user.number === cellphoneNumber) {
 
             user.contacts.forEach((contact) => {
                 
@@ -132,43 +143,38 @@ const getAllMessagesUser = (nickname) => {
                             messageTime
 
                         }
-
                     )
                 })
 
-                message.contacts.push(
-                    {
-                        name,
-                        messages
-                    }
-                )
+                message.contacts.push({ name, messages })
 
             })
 
-        }
+        } 
 
     })
 
-    return message
+    let result = message.contacts.length < 1 ? MESSAGE_ERROR : message 
+    return result
 
 }
 
-const getUserChat = (nickname, contactName) => {
+//Pega a conversa com determinado do usuário
+const getUserChat = (cellphoneNumber, contactNumber) => {
 
     let message = {
         status: true,
         status_code: 200,
         development:"Gabriel Lacerda Correia", 
-        user: {nickname},
         contact: {}
     }
 
     dataUser.contatos['whats-users'].forEach((user) => {
 
-        if (user.nickname === nickname) {
+        if (user.number === cellphoneNumber) {
             user.contacts.forEach((contact) => {
-                if (contact.name === contactName) {
-                    
+                if (contact.number === contactNumber) {
+
                     let messages = []
 
                     contact.messages.forEach((message) => {
@@ -180,20 +186,24 @@ const getUserChat = (nickname, contactName) => {
                         messages.push({ messageSender, messageContent, messageTime })
                 
                     })
-                    message.contact.name = contact.name
-                    message.contact.messages =  messages
 
+
+                    message.contact.name = contact.name
+                    message.contact.messages = messages
                 }
             })
-        }
-
+        } 
     })
 
-    return message
+    let result = dataUser.contatos["whats-users"]
+    .find(user => user.number === cellphoneNumber) ? message : MESSAGE_ERROR
+    
+    return result
 
 }
 
-const searchFilter = (nickname, keyword) => {
+// Procura mensagens de um usuário com uma palavra chave
+const getMessageByKeyword = (cellphoneNumber, keyword) => {
 
     result = {
         status: true,
@@ -204,7 +214,7 @@ const searchFilter = (nickname, keyword) => {
     }
 
     dataUser.contatos['whats-users'].forEach((user) => {
-        if (user.nickname === nickname) {
+        if (user.number === cellphoneNumber) {
             user.contacts.forEach((contact) => {
                 contact.messages.forEach((message) => {
                     if(message.content.toLowerCase().includes(keyword.toLowerCase())) {
@@ -221,18 +231,17 @@ const searchFilter = (nickname, keyword) => {
         }
     })
 
-    return result
+    let messageResult = result.messages.length < 1 ? MESSAGE_ERROR : message
+    return messageResult
 
 }
 
-
-
 // console.log(getAllDataUsers())
-// console.log(getDataUser('Ricky'))
-// console.log(getAllDataContacts('BeeR'))
-// console.log(getAllMessagesUser('Sand'))
-// console.log(getUserChat('Ricky', 'Ana Maria'))
-// console.log(searchFilter('Ricky', 'mark'))
+// console.log(getAllDataUser('11987876567'))
+// console.log(getAllDataContacts('11987876567'))
+// console.log(getAllMessagesUser('11987876567'))
+// console.log(getUserChat('11987876567', '26999999963'))
+// console.log(getMessageByKeyword('11987876567', 'mark'))
 
 module.exports = {
     getAllDataUsers,
@@ -240,5 +249,5 @@ module.exports = {
     getAllDataContacts,
     getAllMessagesUser,
     getUserChat,
-    searchFilter
+    getMessageByKeyword
 }
